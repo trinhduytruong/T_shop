b386c09999e621fe2e5bcbd84820b9b110018e02
intern_truongtd <trinhduytruong02@gmail.com>
2023-09-07 22:36:22 +0700

    test

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from '../components/Login/Login'; 
+
+const loginPage = () => {
+  return (
+    <div className=''>
+        <Login/>
+    </div>
+  )
+}
+
+export default loginPage;
---
+++ frontend/src/server.js
@@ -0,0 +1,1 @@
+export const server =  "http://localhost:3001/api/v2";
